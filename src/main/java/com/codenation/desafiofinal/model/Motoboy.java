553b7965bb40bf795f1238c4079ff3e6e1899fdd
Ricardo Lima <ricardoccofg@gmail.com>
2019-02-22 23:57:49 -0300

    comentario final pra implementar amanhã

--- src/main/java/com/codenation/desafiofinal/model/Motoboy.java
+++ src/main/java/com/codenation/desafiofinal/model/Motoboy.java
@@ -1,11 +1,14 @@
 package com.codenation.desafiofinal.model;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -22,6 +25,9 @@ public class Motoboy {
 	@Embedded
 	private Localizacao localizacao;
 
+	@OneToMany(mappedBy = "motoboy")
+	private List<EntregaPedido> listaEntregas;
+
 	public Motoboy() {
 
 	}
@@ -34,12 +40,20 @@ public class Motoboy {
 	public Long getId() {
 		return id;
 	}
+	public List<EntregaPedido> getListaEntregas() {
+		return listaEntregas;
+	}
 	public Localizacao getLocalizacao() {
 		return localizacao;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+	public void setListaEntregas(List<EntregaPedido> listaEntregas) {
+		this.listaEntregas = listaEntregas;
+	}
+
 	public void setLocalizacao(Localizacao localizacao) {
 		this.localizacao = localizacao;
 	}
